3949ecd8c46c293f4e8b2d33193fe8e4cdced112
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-09-29 20:43:07 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,8 @@
 
 def speak_to_grandma(phrase)
   if phrase == phrase.uppercase
+    "NO, NOT SINCE 1938!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   else
     "HUH?! SPEAK UP, SONNY!"
